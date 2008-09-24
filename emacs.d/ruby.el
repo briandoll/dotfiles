6c2c9594fd10a268dfedf7c96c4b9aef36a6321a
Brian Doll <brian@emphaticsolutions.com>
2008-09-24 14:34:43 -0700

    swapping load order of ruby-mode includes

--- emacs.d/ruby.el
+++ emacs.d/ruby.el
@@ -1,6 +1,6 @@
 ;; loads ruby mode when a .rb file is opened.
-(load-file "~/.emacs.d/modes/inf-ruby.el")
 (load-file "~/.emacs.d/modes/ruby-mode.el")
+(load-file "~/.emacs.d/modes/inf-ruby.el")
 
 (autoload 'ruby-mode "ruby-mode" "Major mode for editing ruby scripts." t)
 (setq auto-mode-alist  (cons '(".rb$" . ruby-mode) auto-mode-alist))
