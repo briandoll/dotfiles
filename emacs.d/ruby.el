a4dbfdc253e1a2d7f8fe741beac8c4f374231db7
Brian Doll <brian@emphaticsolutions.com>
2012-01-02 12:06:22 -0800

    I tried emacs for a while a few years ago. Bad memories. Nevermore.

--- emacs.d/ruby.el
+++
@@ -1,8 +0,0 @@
-;; loads ruby mode when a .rb file is opened.
-(load-file "~/.emacs.d/modes/ruby-mode.el")
-(load-file "~/.emacs.d/modes/inf-ruby.el")
-
-(autoload 'ruby-mode "ruby-mode" "Major mode for editing ruby scripts." t)
-(setq auto-mode-alist  (cons '(".rb$" . ruby-mode) auto-mode-alist))
-(setq auto-mode-alist  (cons '(".erb$" . html-mode) auto-mode-alist))
-
