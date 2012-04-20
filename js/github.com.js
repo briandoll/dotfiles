0f0a97b837c909520d0166f014a4f62bdad5fc16
Brian Doll <brian@emphaticsolutions.com>
2012-04-19 20:09:07 -0700

    remove custom github js

--- js/github.com.js
+++
@@ -1,6 +0,0 @@
-var hideRails = function(){
-  $('.alert .body .title a:contains("rails/rails")')
-    .parent('.title').parent('.body').parent('.alert').hide();
-};
-$('ul.tabs').append('<li><a href="#" id="hiderails">Hide Rails</a></li>');
-$('#hiderails').click(hideRails);
